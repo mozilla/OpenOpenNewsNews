079a6a191c5511f363cdf7a48e804aba2cbe8376
Brian Abelson <brianabelson@gmail.com>
2013-06-22 20:22:06 -0400

    added stopwords

--- get_homepages.py
+++ get_homepages.py
@@ -1,6 +1,7 @@
 import pandas
 import requests
 import re
+import itertools
 from datetime import datetime
 master_csv = 'https://docs.google.com/spreadsheet/pub?key=0An9Q5Mkz4lG7dGt6ZTNZLU03cnZRMkQtMkdaZ1lhd1E&single=true&gid=0&output=csv'
 d = pandas.read_csv(master_csv)
@@ -13,3 +14,5 @@ for u in d['site_url']:
   html = requests.get(u).text
   f.write(html.encode('utf-8'))
   f.close()
+
+def
